1cb1f9368fcb1d06b7f913eafddf0ea8c8049924
Katie Brown <katieleebrown19@gmail.com>
2022-08-25 10:48:40 -0400

    hero section created

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,16 @@
+import heroImage from './images/heroImages.png'
+
 export default function Hero() {
     return (
-        <div>
+        <section id="hero">
+            <div id='heroImages' className="">
+                <img src={heroImage} alt='a collection of images highlighting some of the experiences you can book through airbnb, including cooking, dancing, swimming, and more.' />
+            </div>
 
-        </div>
+            <div id='heroText'>
+                <h1>Online Experiences</h1>
+                <p>Join unique interactive activities led by one-of-a-kind hosts - all without leaving home.</p>
+            </div>
+        </section>
     )
 }
